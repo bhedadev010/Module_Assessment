c08d6fbb06e5d72f7b701fa9385a51e822ac4cdd
Dev Bheda <bhedadev010@gmail.com>
2024-08-12 09:18:21 +0530

    12-8-24

--- Module-1 (Introduction To Python)/assessment_1.py
+++ Module-1 (Introduction To Python)/assessment_1.py
@@ -4,6 +4,7 @@ def f_astock(f_name, f_qty, f_price):
     f_details['qty'] = f_qty
     f_details['price'] = f_price
     stock[f_name] = f_details
+    print("\n ACTION IS SUCCESSFUL \n")
 
     file = open("log_file.txt", "a")
     file.write(f"\n{f_name} => Quantity : {f_qty} , Price : {f_price}   ({datetime.datetime.now()})\n*****************************************************************************************************")
@@ -13,10 +14,22 @@ def f_ustock(f_name, f_qty, f_price):
     f_details['qty'] = f_qty
     f_details['price'] = f_price
     stock[f_name] = f_details
+    print("\n ACTION IS SUCCESSFUL \n")
+
     file = open("log_file.txt", "a")
     file.write(f"\n{f_name} => Quantity : {f_qty} , Price : {f_price}   ({datetime.datetime.now()})     [UPDATED]\n*****************************************************************************************************")
     file.close()
 
+def c_buy(b_name, b_qty, price, name):
+    file = open("log_file.txt", "a")
+    file.write(f"\n{b_name} => Quantity : {b_qty} , Bill : {price},  Customer : {name}   ({datetime.datetime.now()})     [PURCHASED]\n*****************************************************************************************************")
+    file.close()
+    upd(f_name,f_qty, f_price)
+
+def upd(name, qty, price):
+    file = open("log_file.txt", "a")
+    file.write(f"\n{f_name} => Quantity : {f_qty - b_qty} , Price : {f_price}   ({datetime.datetime.now()})     [STOCK]\n*****************************************************************************************************")
+    file.close()
 
 # dictionary to store details
 
@@ -43,47 +56,47 @@ while True:
 
 
             if choice == '1':
-                f_name = input("Enter Fruit Name :")
+                f_name = input("Enter Fruit Name :").upper()
                 f_qty = int(input("Enter qty(in kg) :"))
                 f_price = int(input("Enter Price :"))
                 f_astock(f_name, f_qty, f_price)
-                choi = input("Do you want to perform any more operations. press y for yes n for no :")
-                if choi == 'n':
+                choice2 = input("Do you want to perform any more operations. press y for yes n for no :")
+                if choice2 == 'n':
                     break
-                elif choi == "y":
+                elif choice2 == "y":
                     continue
                 else:
                     print("!!!INVALID INPUT!!!")
 
             elif choice == '2':
                 print(stock)
-                choi = input("Do you want to perform any more operations. press y for yes n for no :")
-                if choi == 'n':
+                choice2 = input("Do you want to perform any more operations. press y for yes n for no :")
+                if choice2 == 'n':
                     break
-                elif choi == "y":
+                elif choice2 == "y":
                     continue
                 else:
                     print("!!!INVALID INPUT!!!")
 
             elif choice == '3':
-                upd = input("Enter Fruit Name :")
+                upd = input("Enter Fruit Name :").upper()
                 if upd in stock:
                     f_qty = int(input("Enter qty(in kg) :"))
                     f_price = int(input("Enter Price :"))
                     f_ustock(upd, f_qty, f_price)
-                    choi = input("Do you want to perform any more operations. press y for yes n for no :")
-                    if choi == 'n':
+                    choice2 = input("Do you want to perform any more operations. press y for yes n for no :")
+                    if choice2 == 'n':
                         break
-                    elif choi == "y":
+                    elif choice2 == "y":
                         continue
                     else:
                         print("!!!INVALID INPUT!!!")
                 else:
                     print("not in database")
-                    choi = input("Do you want to perform any more operations. press y for yes n for no :")
-                    if choi == 'n':
+                    choice2 = input("Do you want to perform any more operations. press y for yes n for no :")
+                    if choice2 == 'n':
                         break
-                    elif choi == "y":
+                    elif choice2 == "y":
                         continue
                     else:
                         print("!!!INVALID INPUT!!!")
@@ -92,9 +105,69 @@ while True:
 
 
     elif role == '2':
-        print("Hello!")
+
+        while True:
+
+            print("\n\t\t\tCustomer\n")
+            print("\t\t\t 1) View Fruit Market")
+            print("\t\t\t 2) Exit\t\t\t\n\n")
+
+            choice3 = input("Enter Your Choice :")
+
+            if choice3 == '1':
+                print("\n\t\t\tFruit Market")
+                for i in stock.keys():
+                    print(f"\n\t\t\t{i} : {stock[i]}")
+
+                print("\n1) Buy Fruits")
+                print("2) Exit\n")
+                choice4 = input("Enter Your Choice :")
+
+                if choice4 == '1':
+                    b_name = input("Enter Fruit Name :").upper()
+                    if b_name in stock:
+                        b_qty = int(input("Enter qty(in kg) :"))
+                        if b_qty <= stock[b_name]["qty"]:
+                            stock[b_name]["qty"] -= b_qty
+                            price = b_qty*stock[b_name]["price"]
+
+                            print("\n\t\t\tYour Bill\t\t\t\n")
+                            name = input("Enter Your Name :")
+
+                            print(f"{b_name} : {b_qty} (qty)  => Rs. {price}")
+                            c_buy(b_name, b_qty, price, name)
+
+                            print("\t\t\tThank You For Shopping\t\t\t\n")
+                            choice5 = input("Do you want to perform any more operations. press y for yes n for no :")
+                            if choice5 == 'n':
+                                break
+                            elif choice5 == "y":
+                                continue
+                            else:
+                                print("!!!INVALID INPUT!!!")
+
+                        else:
+                            print("\nAmount Of Quantity Is Not Available In Market")
+
+                    else:
+                        print("not in market\n")
+
+
+                elif choice4 == '2':
+                    break
+                else:
+                    print("!!! Invalid Input !!!")
+                    continue
+
+            elif choice3 == '2':
+                break
+            else:
+                print("!!! Invalid Input !!!")
+                continue
+
     elif role == '3':
         break
+
     else:
         print("!!! Invalid Input !!!")
         continue
