4249ca2d333ec6142dca77ab47c492c195e3da62
Dev Bheda <bhedadev010@gmail.com>
2024-08-17 16:48:25 +0530

    17-8-24

---
+++ Module-7 (DA)/numpy/Question-3.py
@@ -0,0 +1,7 @@
+# Get all items between 5 and 10 from a.
+
+import numpy as np
+
+a = np.array([2, 6, 1, 9, 10, 3, 27])
+
+#not taught yet
---
+++ Module-7 (DA)/numpy/Question-4.py
@@ -0,0 +1,7 @@
+#Limit the number of items printed in python NumPy array a to a maximum of 6 elements.
+# Input:
+# array([ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
+
+import numpy as np
+
+#not taught
