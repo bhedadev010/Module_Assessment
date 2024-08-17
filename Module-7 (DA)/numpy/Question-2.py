cb481a27bd81768c5f6d0f2b3a391f3313ec6cea
Dev Bheda <bhedadev010@gmail.com>
2024-08-17 16:37:37 +0530

    17-8-24

---
+++ Module-7 (DA)/numpy/Question-2.py
@@ -0,0 +1,10 @@
+# Get the common items between a and b
+
+import numpy as np
+
+a = np.array([1, 2, 3, 2, 3, 4, 3, 4, 5, 6])
+b = np.array([7, 2, 10, 2, 7, 4, 9, 4, 9, 8])
+
+common_items = np.intersect1d(a, b)
+
+print(common_items)
